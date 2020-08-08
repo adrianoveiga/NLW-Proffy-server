ec8169d4e1861d9f9ecc962e7ffe32603318ef17
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-08 02:25:00 -0300

    [code] optmizations: - using transaction to commit all database operations at one time - using try/catch when trying to insert data on database, calling rollback if any error occurred - api response returns now HTTP codes for success (201) and error (400)

--- src/routes.ts
+++ src/routes.ts
@@ -21,37 +21,50 @@ routes.post('/classes', async (request, response) => {
         schedule
     } = request.body;
 
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
+    // open transaction on database to run or rollback all actions together
+    const trx = await db.transaction();
 
-    const user_id = insertedUsersIds[0];
+    try {
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id
+        });
+    
+        const class_id = insertedClassesIds[0];
+    
+        // get the full array of json objects from request body and create
+        // a new map with computed data to the class schedule
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to),
+            };
+        });
+    
+        await trx('class_schedule').insert(classSchedule);
+    
+        await trx.commit();  // apply all inserts on database
+    
+        return response.status(201).send();
+    } catch (err) {
+        await trx.rollback();
 
-    const insertedClassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    // get the full array of json objects from request body and create
-    // a new map with computed data to the class schedule
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to),
-        };
-    });
-
-    await db('class_schedule').insert(classSchedule);
-
-    return response.send();
+        return response.status(400).json({
+            error: 'Unexpected error while creating new class'
+        })
+    }
 });
 
 export default routes;
