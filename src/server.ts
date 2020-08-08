b01d85a15b2914261eb503b571c79674c9f85e65
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-07 23:39:59 -0300

    [cod] simple api json endpoint with express

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-    return response.send("Hello, World!");
+    const users = [
+        { name: 'Adriano', age: 32 },
+        { name: 'John Doe', age: 40 },
+    ]
+    return response.json(users);
 });
 
 app.listen(3333);
