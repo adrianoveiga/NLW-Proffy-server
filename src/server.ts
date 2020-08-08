b10900fd9f15656134ede509d8b5bd731f7be8ad
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-07 23:37:13 -0300

    [cod][requirements] add express framework to create server HTTP responses and routing, with a Hello World example

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!");
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    return response.send("Hello, World!");
+});
+
+app.listen(3333);
