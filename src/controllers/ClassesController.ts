f6a8ee57a218ef8adc7746dc54ee55232f6da9c0
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-08 03:00:19 -0300

    [code] creating a new GET route for new method on ClassesController to list all classes with filters, applying an join (inner join) with users table to get all information related from the classes

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -10,6 +10,29 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+    async index(request: Request, response: Response) {
+        const filters = request.query;
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        if (!week_day || !subject || !time) {
+            return response.status(400).json({
+                error: 'Missing filters to search classes'
+            });
+        }
+
+        const timeInMinutes = convertHourToMinutes(time);
+
+        const classes = await db('classes')
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
+
+        return response.json(classes);
+    }
+
     async create (request: Request, response: Response) {
         const {
             name,
