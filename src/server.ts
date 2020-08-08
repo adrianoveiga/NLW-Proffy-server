3bbec1686ddc1ef155844f263f37ea07ca66aaad
Adriano Veiga Botelho <adriano.botelho@lactec.org.br>
2020-08-08 03:52:12 -0300

    [code][requirements] add Cors to project to allow connections from frontend on different addresses (ex: localhost:3000 and localhost:3333)

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+// CORS - needed to connect our server with frontend and app running
+//        on different addresses (ex: localhost:3000 and localhost:3333)
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
